4b80213f837a65af8c1dfba71c97a9bd6cef58ee
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-09-04 14:20:52 -0500

    Fixed contact form back end

--- client/src/components/layout/Contact.js
+++ client/src/components/layout/Contact.js
@@ -1,124 +1,127 @@
-import React from 'react'
-import { Form, FormGroup, Input, Label, Button } from 'reactstrap'
+import React, { useState } from 'react'
+import { Form, FormGroup, Input, Label, Button, Alert } from 'reactstrap'
 import axios from 'axios'
 import '../../css/Contact.css'
 
-class Contact extends React.Component {
+const Contact = () => {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    message: '',
+    status: null
+  })
 
-  constructor() {
-    super()
-
-    this.state = {
-      name: '',
-      email: '',
-      message: ''
-    }
-
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+  const onChange = e => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
   }
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-  async handleSubmit(e) {
-    e.preventDefault();
-
-    const { name, email, message} = this.state;
+  const onSubmit = async e => {
+    e.preventDefault()
 
     const config = {
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
+        'Content-Type': 'application/json'
       }
     }
 
-    await axios.post('/api/form', {
-      name,
-      email,
-      message
-    }, config)
+    const formData = {
+      name: state.name,
+      email: state.email,
+      message: state.message
+    }
+
+    const res = await axios.post('http://localhost:5000/api/form', formData, config)
+
+    if(res.status === 200)
+      setState({
+        ...state,
+        name: '',
+        email: '',
+        message: '',
+        status: 200
+      })
   }
 
-  render() {
-    return (
-      <div className="contact-grid">
-        <div className="contact-page-title">
-          <h1>Contact Me</h1>
-        </div>
+  return (
+    <div className="contact-grid">
+      <div className="contact-page-title">
+        <h1>Contact Me</h1>
+      </div>
 
-        <div className="contact-page-description">
-          <p>
-            If you have any questions over the content of the website feel free
-            to contact me! I will answer any questions as soon as I can.
-          </p>
-          <br/>
-          <p>
-            Also, if there are any mistakes such as: errors in the code, grammatical
-            errors in the text or errors loading content through links or images,
-            let me know.
-          </p>
-        </div>
+      <div className="contact-page-description">
+        <p>
+          If you have any questions over the content of the website feel free
+          to contact me! I will answer any questions as soon as I can.
+        </p>
+        <br/>
+        <p>
+          Also, if there are any mistakes such as: errors in the code, grammatical
+          errors in the text or errors loading content through links or images,
+          let me know.
+        </p>
+      </div>
 
-        <div className="container">
-          <div className="wrapper animated bounceInLeft">
+      <div className="container">
+        <div className="wrapper animated bounceInLeft">
 
-            <div className="personal-info">
-              <h3>My Info</h3>
-              <ul>
-                <li>
-                  <p>Name: </p>
-                  Cameron Shatto
-                </li>
-                <li>
-                  <p>Email: </p>
-                  ExploringDataStructures@gmail.com
-                </li>
-              </ul>
-            </div>
-            <div className="contact">
-              <Form onSubmit={this.handleSubmit}>
-                <FormGroup className="form-group">
-                  <Label for="name">Name:</Label>
-                  <Input
-                    type="text"
-                    name="name"
-                    onChange={this.handleChange} />
-                </FormGroup>
+          <div className="personal-info">
+            <h3>My Info</h3>
+            <ul>
+              <li>
+                <p>Name: </p>
+                Cameron Shatto
+              </li>
+              <li>
+                <p>Email: </p>
+                ExploringDataStructures@gmail.com
+              </li>
+            </ul>
+          </div>
+          <div className="contact">
+            <Form onSubmit={e => onSubmit(e)}>
+              <FormGroup className="form-group">
+                <Label for="name">Name:</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  value={state.name}
+                  onChange={e => onChange(e)} required/>
+              </FormGroup>
 
-                <FormGroup className="form-group">
-                  <Label for="email">Email:</Label>
-                  <Input
-                    type="email"
-                    name="email"
-                    onChange={this.handleChange} />
-                </FormGroup>
+              <FormGroup className="form-group">
+                <Label for="email">Email:</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  value={state.email}
+                  onChange={e => onChange(e)} required/>
+              </FormGroup>
 
-                <FormGroup className="full">
-                  <Label for="message">Message:</Label>
-                  <Input
-                    type="textarea"
-                    name="message"
-                    onChange={this.handleChange} />
-                </FormGroup>
+              <FormGroup className="full">
+                <Label for="message">Message:</Label>
+                <Input
+                  type="textarea"
+                  name="message"
+                  value={state.message}
+                  onChange={e => onChange(e)} required/>
+              </FormGroup>
 
-                <Button className="full">Send Email</Button>
-              </Form>
-            </div>
+              {state.status === 200 && <Alert className="full" color="success">Email sent!</Alert>}
+              <Button className="full">Send Email</Button>
+            </Form>
           </div>
         </div>
-
-        <div className="contact-page-hr">
-          <hr/>
-        </div>
-
       </div>
-    )
-  }
 
+      <div className="contact-page-hr">
+        <hr/>
+      </div>
+
+    </div>
+  )
 }
 
 export default Contact
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.post('/api/form', async (req, res) => {
   const htmlEmail = `
